da81a6f371a4a9351b838fb9bdaf29f9bcf2a1d6
Matthew Nichols <matthew@matthew-nichols.com>
2016-07-05 13:03:53 -0600

    Got react build working

--- client-src/js/index.js
+++ client-src/js/index.js
@@ -1,4 +1,10 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import Hello from './components/hello';
+
 require("../style/main.scss");
 
 let name = "Matthew";
 console.log(`Hi ${name}`);
+
+ReactDom.render(<Hello></Hello>, document.getElementById('appContainer'))
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         loaders: [
 
             {
-                test: /\.js/,
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
